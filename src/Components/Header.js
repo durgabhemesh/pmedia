74d46242d62b0baf369665e49cd0e61d1935fa18
“durgabhemesh.bagu@gmail.com” <durgabhemesh.bagu@gmail.com>
2023-09-16 12:13:38 +0530

    sending to pravin

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+   
+  return (
+    <div className='header'>
+<div className='title'>
+    <h4 style={{display: 'inline',paddingTop:'5px',color:'black'}}> Placement's Media</h4>
+    <p style={{fontSize: '11px'}}>Building Trust and Careers...</p>
+    </div>
+
+   
+        
+    </div>
+
+  )
+}
+
+export default Header
---
+++ src/Components/Items.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Items.css'
+
+const Items = () => {
+  return (
+    <div>
+       <div className='Container'>
+        <a href='https://www.w3schools.com/js/js_date_methods.asp'>
+        <div className='Card'>
+            { <img src=''></img>}
+           
+            <h1>TCS</h1>
+            <p>Tcs is hiring</p>
+            {/* <button onClick={()=>props.cvalue(props.id)}>Add To Cart</button>  */}
+        </div>
+        </a>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Items
